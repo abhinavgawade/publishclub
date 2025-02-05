56ff64333d4d0d31ad3d4faf29ca24da3c6a46f6
abhinavgawade <140579103+abhinavgawade@users.noreply.github.com>
2025-02-05 21:13:42 +0530

    Add files via upload

---
+++ homepage.php
@@ -0,0 +1,256 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Home Page | Blog Website</title>
+    <link rel="stylesheet" href="style.css" />
+    <script
+      src="https://kit.fontawesome.com/7a4b62b0a4.js"
+      crossorigin="anonymous"
+    ></script>
+  </head>
+  <body>
+    <header>
+      <nav>
+        <h1>The Publish Club🪶</h1>
+        
+        <ul>
+          <a href="home.html">
+            <li>Home</li>
+          </a>
+          <li>Posts</li>
+          <a href="login.php">
+            <li>Login</li>
+          </a>
+          <li>About</li>
+          <li>
+            <i class="fa fa-instagram"></i>
+          </li>
+          <li>
+            <i class="fa fa-twitter"></i>
+          </li>
+          <li>
+            <i class="fab fa-linkedin"></i>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <section id="hero">
+        <div class="hero_content">
+          <h1>I'm not addicted to reading!</h1>
+          <h4>I can quit as soon as I finish just one more blog... or maybe two."</h4>
+        </div>
+        <div class="hero_image">
+          <img src="images/ban.png" alt="hero" />
+        </div>
+      </section>
+
+    <section class="editor-picks">
+        <h2>Editor's picked</h2>
+        <p>Featured and highly rated articles</p>
+
+        <div class="articles">
+            <!-- First Article Card -->
+            <div class="article-card">
+                <img src="images/edit.jpg" alt="City at Night">
+                <div class="article-info">
+                    <span class="tags">Spiritual</span>
+                </div>
+            </div>
+
+            <!-- Second Article Card -->
+            <div class="article-card">
+                <img src="images/history.png" alt="Eagle in Flight">
+                <div class="article-info">
+                    <span class="tags">History</span>
+                </div>
+            </div>
+            <div class="article-card">
+                <img src="images/MENTAL-HEALTH.jpg" alt="Eagle in Flight">
+                <div class="article-info">
+                    <span class="tags">Psychology</span>
+                </div>
+            </div>
+            <div class="article-card">
+                <img src="images/lite.jpg" alt="Eagle in Flight">
+                <div class="article-info">
+                    <span class="tags">Literature</span>
+                </div>
+            </div>
+            <div class="article-card">
+                <img src="images/reev.jpg" alt="Eagle in Flight">
+                <div class="article-info">
+                    <span class="tags">Revolutionary</span>
+                </div>
+            </div>
+            <div class="article-card">
+                <img src="images/caste.jpg" alt="Eagle in Flight">
+                <div class="article-info">
+                    <span class="tags">Caste in India</span>
+                </div>
+            </div>
+        </div>
+        <div class="articles">
+            <!-- First Article Card -->
+            <div class="article-card">
+                <img src="images/maharaj.jpg" alt="City at Night">
+                <div class="article-info">
+                    <span class="tags">Poem</span>
+                </div>
+            </div>
+
+            <!-- Second Article Card -->
+            <div class="article-card">
+                <img src="images/patelji.jpg" alt="Eagle in Flight">
+                <div class="article-info">
+                    <span class="tags">Unity</span>
+                </div>
+            </div>
+            <div class="article-card">
+                <img src="images/mahatma.jpg" alt="Eagle in Flight">
+                <div class="article-info">
+                    <span class="tags">Freedom Struggle</span>
+                </div>
+            </div>
+            <div class="article-card">
+                <img src="images/cric.jpg" alt="Eagle in Flight">
+                <div class="article-info">
+                    <span class="tags">God of Cricket</span>
+                </div>
+            </div>
+            <div class="article-card">
+                <img src="images/stock.jpeg" alt="Eagle in Flight">
+                <div class="article-info">
+                    <span class="tags">Stock Market</span>
+                </div>
+            </div>
+            <div class="article-card">
+                <img src="images/ai.jpeg" alt="Eagle in Flight">
+                <div class="article-info">
+                    <span class="tags">Ai is Future</span>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+      <div class="h-card">
+        <div class="card_content1">
+          <h2>Participate in our new Quiz Round!!!!!!</h2>
+          <p>
+            for more Details
+            <a href="post.html"
+                  ><button class="read-btn">Read More</button>
+            </a>
+          </p>
+          <div class="status">
+            <p>4 Nov 2024</p>
+            <a href="#.html"><button class="read-btn"></button></a>
+          </div>
+        </div>
+        <img src="images/quiz.jpeg" alt="laptop guy" />
+      </div>
+      <button class="post-button">Post Something</button>
+
+
+      <div class="yourrecentsearch">
+        <h2> Your Recent Searches.</h2>
+      </div>
+      <div class="card_container">
+        <div class="card_wrapper">
+          <div class="v-card">
+            <div class="card_content">
+              <h2>Heavy Internet</h2>
+              <p>
+                It is a long established fact that a reader will be distracted
+                by the readable content of a page when looking at its layout.
+                The point of using Lorem Ipsum is that....
+              </p>
+              <div class="status">
+                <p>8 Feb 2022</p>
+                <a href="post.html"
+                  ><button class="read-btn">Read More</button>
+                </a>
+              </div>
+            </div>
+            <img src="images/card1.png" alt="laptop guy" />
+          </div>
+        </div>
+        <div class="card_wrapper">
+          <div class="v-card">
+            <div class="card_content">
+              <h2>Heavy Internet</h2>
+              <p>
+                It is a long established fact that a reader will be distracted
+                by the readable content of a page when looking at its layout.
+                The point of using Lorem Ipsum is that....
+              </p>
+              <div class="status">
+                <p>8 Feb 2022</p>
+                <a href="post.html"
+                  ><button class="read-btn">Read More</button></a
+                >
+              </div>
+            </div>
+            <img src="images/card1.png" alt="laptop guy" />
+          </div>
+        </div>
+        <div class="card_wrapper">
+          <div class="v-card">
+            <div class="card_content">
+              <h2>Heavy Internet</h2>
+              <p>
+                It is a long established fact that a reader will be distracted
+                by the readable content of a page when looking at its layout.
+                The point of using Lorem Ipsum is that....
+              </p>
+              <div class="status">
+                <p>8 Feb 2022</p>
+                <a href="post.html"
+                  ><button class="read-btn">Read More</button></a
+                >
+              </div>
+            </div>
+            <img src="images/card1.png" alt="laptop guy" />
+          </div>
+        </div>
+      </div>
+      <div class="h-card">
+        <div class="card_content">
+          <h2>Get Connected to us</h2>
+          <p>
+            It is a long established fact that a reader will be distracted by
+            the readable content of a page when looking at its layout. The point
+            of using that....
+          </p>
+          <div class="status">
+            <p>4 Nov 2024</p>
+            <a href="post.html"><button class="read-btn">Read More</button></a>
+          </div>
+        </div>
+        <img src="images/stay.jpeg" alt="laptop guy" />
+      </div>
+    </main>
+ 
+    
+    <hr />
+    <footer>
+      <p>The Publish Club 2024 copyright all rights reserved</p>
+
+      <ul>
+        <li>
+          <i class="fa fa-instagram"></i>
+        </li>
+        <li>
+          <i class="fa fa-twitter"></i>
+        </li>
+        <li>
+          <i class="fab fa-linkedin"></i>
+        </li>
+      </ul>
+    </footer>
+  </body>
+</html>
